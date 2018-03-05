e7177837bfad8787168f581b048921e25356e5ac
Yoann Brosseau <yoann.brosseau@gmail.com>
2018-03-05 09:49:03 +0100

    add chars...

--- src/kaleidoscope/lang/FR-bépo-celtic.h
+++ src/kaleidoscope/lang/FR-bépo-celtic.h
@@ -1,4 +1,42 @@
 #pragma once
 
 // Alt gr
-#define ALTGR RCTRL
+#frbfine ALTGR RALT
+
+#frbfine Key_frb_1 Key_1
+#frbfine Key_frb_2 Key_2
+#frbfine Key_frb_3 Key_3
+#frbfine Key_frb_4 Key_4
+#frbfine Key_frb_5 Key_5
+#frbfine Key_frb_6 Key_6
+#frbfine Key_frb_7 Key_7
+#frbfine Key_frb_8 Key_8
+#frbfine Key_frb_9 Key_9
+#frbfine Key_frb_0 Key_0
+
+#frbfine Key_frb_A Key_A
+#frbfine Key_frb_B Key_Q
+#frbfine Key_frb_C Key_H
+#frbfine Key_frb_D Key_U
+#frbfine Key_frb_E Key_F
+#frbfine Key_frb_F Key_Slash
+#frbfine Key_frb_G Key_M
+#frbfine Key_frb_H Key_Comma
+#frbfine Key_frb_I Key_D
+#frbfine Key_frb_J Key_U
+#frbfine Key_frb_K Key_K
+#frbfine Key_frb_L Key_O
+#frbfine Key_frb_M Key_Quote
+#frbfine Key_frb_N Key_Semicolon
+#frbfine Key_frb_O Key_R
+#frbfine Key_frb_P Key_E
+#frbfine Key_frb_Q Key_M
+#frbfine Key_frb_R Key_L
+#frbfine Key_frb_S Key_K
+#frbfine Key_frb_T Key_J
+#frbfine Key_frb_U Key_S
+#frbfine Key_frb_V Key_U
+#frbfine Key_frb_W Key_RightBracket
+#frbfine Key_frb_X Key_C
+#frbfine Key_frb_Y Key_X
+#frbfine Key_frb_Z Key_LeftBracket
